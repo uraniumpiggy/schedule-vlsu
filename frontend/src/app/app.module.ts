7ba02867b8f570d0d978ff314ba2f0cef567bd2f
Alex Luibeznov <dr3amyh3ad@gmail.com>
2022-02-12 16:56:20 +0300

    create main component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppbarComponent } from './appbar/appbar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
