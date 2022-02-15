cf7a48e8cc4aa30ae7c6ec2f925cc92d9582024e
Alex Luibeznov <dr3amyh3ad@gmail.com>
2022-02-15 16:49:46 +0300

    in app.module.ts: add calendar component; in app.component:

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,17 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppbarComponent } from './appbar/appbar.component';
 import { ContentComponent } from './content/content.component';
+import { GroupsComponent } from './groups/groups.component';
+import { CalendarComponent } from './calendar/calendar.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AppbarComponent,
-    ContentComponent
-  ],
+    ContentComponent,
+    GroupsComponent,
+    CalendarComponent
+],
   imports: [
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
