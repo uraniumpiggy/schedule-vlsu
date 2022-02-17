832b3b001604aed1009ec7b011080dd84bb99f79
Alex Luibeznov <dr3amyh3ad@gmail.com>
2022-02-17 16:40:26 +0300

    fixed button

--- frontend/src/app/appbar/button/button.component.ts
+++ frontend/src/app/appbar/button/button.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 export class ButtonComponent {
 
+    title = "Группа"
     toggleAppbarColor = false;
     @Output() toggle: EventEmitter<boolean> = new EventEmitter<boolean>();
 
