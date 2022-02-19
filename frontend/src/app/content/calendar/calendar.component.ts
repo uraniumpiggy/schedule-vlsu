1e9286a171bef1cafedb2b28e33ec644a33a30d1
Alex Luibeznov <dr3amyh3ad@gmail.com>
2022-02-19 22:05:38 +0300

    create calendar card

--- frontend/src/app/content/calendar/calendar.component.ts
+++ frontend/src/app/content/calendar/calendar.component.ts
@@ -3,14 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styles: [
-  ]
+  styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class CalendarComponent {
 
 }
